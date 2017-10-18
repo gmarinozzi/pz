427742a4e9ab5eb01a0d037dea36d691b2f1f8ec
gmarinozzi <gmarinozzi@gmail.com>
2017-10-17 20:18:42 -0500

    Automated commit at 2017-10-18 01:18:42 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -52,25 +52,6 @@ $(document).ready(function() {
       }
     });
   });
-}); 
-SCROLL
-//modal
-$(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
-
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
 });
 
 //ranges
